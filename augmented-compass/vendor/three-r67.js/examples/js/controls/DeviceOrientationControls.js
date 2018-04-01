278d0b5dc2966044a14c001b56c968e70339f2a4
Shivank Shekhar <shivank1995@gmail.com>
2018-04-01 14:53:15 +0530

    Update DeviceOrientationControls.js

--- augmented-compass/vendor/three-r67.js/examples/js/controls/DeviceOrientationControls.js
+++ augmented-compass/vendor/three-r67.js/examples/js/controls/DeviceOrientationControls.js
@@ -1,10 +1,3 @@
-/**
- * @author richt / http://richt.me
- * @author WestLangley / http://github.com/WestLangley
- *
- * W3C Device Orientation control (http://w3c.github.io/deviceorientation/spec-source-orientation.html)
- */
-
 THREE.DeviceOrientationControls = function ( object ) {
 
 	this.object = object;
