5c888794e8c9eca4fe94fb91c8c2da68ac4340a7
GeekyShiva <shivank1995@gmail.com>
2018-04-01 14:56:02 +0530

    update

--- augmented-compass/TODO.md
+++ augmented-compass/TODO.md
@@ -3,4 +3,4 @@
 - implement fullscreen on click
 - implement a notification when used on desktop
 - make the geometry less expensive ?
-  - works well on my personnal phone... good enougth ?
+  - works well on my personal phone... good enough ?
--- augmented-compass/vendor/three.js/examples/js/controls/OrbitControls.js
+++ augmented-compass/vendor/three.js/examples/js/controls/OrbitControls.js
@@ -1,10 +1,3 @@
-/**
- * @author qiao / https://github.com/qiao
- * @author mrdoob / http://mrdoob.com
- * @author alteredq / http://alteredqualia.com/
- * @author WestLangley / http://github.com/WestLangley
- * @author erich666 / http://erichaines.com
- */
 /*global THREE, console */
 
 // This set of controls performs orbiting, dollying (zooming), and panning. It maintains
